30605403bee14d4a1fec1f5cb334756ee7ab2918
Aman Tiwari <tiwari.aman85@gmail.com>
2020-02-19 00:28:57 +0530

    [fix] handled corrupted files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 from sketch_algo1 import image_algo_one
 from sketch_algo2 import image_algo_two
 import click
+from PIL import Image
+
 
 @click.command()
 @click.option('--source_dir', required=True, type=str)
@@ -10,18 +12,24 @@ def generate(source_dir):
     dir = source_dir
     for root, dirs, files in os.walk(dir):
         for each in files:
+
             print("Processing : {}".format(each))
             filename, extension = each.split(".")
-            image_path = os.path.join(root, each)
-            sketch_1 = image_algo_one(image_path)
-            sketch_1_filename = filename + "_sketch1." + extension
-            sketch_1_dir = os.path.join("./output", sketch_1_filename)
-            print (sketch_1_dir)
-            cv2.imwrite(sketch_1_dir, sketch_1)
-            sketch_2 = image_algo_two(image_path)
-            sketch_2_filename = filename + "_sketch2." + extension
-            sketch_2_dir = os.path.join("./output", sketch_2_filename)
-            cv2.imwrite(sketch_2_dir, sketch_2)
+            try:
+                image_path = os.path.join(root, each)
+                img = Image.open(image_path)
+                img.verify()  # verify that it is a non corrupted image
+                sketch_1 = image_algo_one(image_path)
+                sketch_1_filename = filename + "_sketch1." + extension
+                sketch_1_dir = os.path.join("./output", sketch_1_filename)
+                print(sketch_1_dir)
+                cv2.imwrite(sketch_1_dir, sketch_1)
+                sketch_2 = image_algo_two(image_path)
+                sketch_2_filename = filename + "_sketch2." + extension
+                sketch_2_dir = os.path.join("./output", sketch_2_filename)
+                cv2.imwrite(sketch_2_dir, sketch_2)
+            except (IOError, SyntaxError) as e:
+                print('Bad file:', filename)
 
 
 if __name__ == '__main__':
